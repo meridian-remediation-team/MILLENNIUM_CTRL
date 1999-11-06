30fb5492526172cd3305bd45d51d32e676c9dea7
Meridian Remediation Team <noreply@meridianremediationteam.com>
1999-11-06 21:33:04 +0200

    legacy code support

---
+++ verify_rollover_legacy.sh
@@ -0,0 +1,6 @@
+# verify_rollover_legacy.sh
+# Deprecated. Replaced by verify_rollover.sh v3.1.
+# Do not use.
+#
+# 			 	 	 		 			  			  	  		  	 	 		    	 		   		 		 	    		    	 		   	  		 		   		  	 	
+#
