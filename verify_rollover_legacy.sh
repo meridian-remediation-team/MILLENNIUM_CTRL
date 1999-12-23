10cc1820e02358524c0e2c6a63f71bd64f140d4e
Meridian Remediation Team <noreply@meridianremediationteam.com>
1999-12-23 12:33:23 +0200

    final status before rollover

--- verify_rollover_legacy.sh
+++ verify_rollover_legacy.sh
@@ -1,6 +1,4 @@
 # verify_rollover_legacy.sh
 # Deprecated. Replaced by verify_rollover.sh v3.1.
 # Do not use.
-#
-# 			 	 	 		 			  			  	  		  	 	 		    	 		   		 		 	    		    	 		   	  		 		   		  	 	
-#
+ 	 	 	   	  				 	   	   	  				  			 	   	      			  	  		  	 	 		 		 	 		 				 			 		  		  	 	  	      		 	    		 	  	 		  	   		  	   		  	 	 		 			   	 				 			  		 		  	 	 		   		 			  	  		  	 	 			 	    	      		  		  			  	  		 				 		 		 	  	      			  	  		  	 	 		  		  			  		
